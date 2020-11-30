1c935e4200d03474a6f7f7b173be0f05238ea063
tehnplk <tehnplk@gmail.com>
2020-11-30 15:33:31 +0700

    tehnn

--- test.py
+++ test.py
@@ -21,9 +21,8 @@ def push_alert(_token, _title, _msg_body):
 
 
 if __name__ == '__main__':
-    _title = f"ทดสอบ3 อีก 1 คิวจะถึงคิวของท่าน"
-    _msg_body = f"""หมายเลข A000 กรุณาไปรอที่บริเวณ จุดทดสอบ3"""
+    _title = f"ทดสอบ อีก 1 คิวจะถึงคิวของท่าน"
+    _msg_body = f"""หมายเลข NA1001 กรุณาไปรอที่บริเวณ จุดทดสอบ3"""
     _token = "fsesCPEAFf0:APA91bG95AnMCWUkofrjtXIG3zjJkbfzkOZuvTXceTf3j30rV4FBdFJZt5oYVSNae_PD8N_B0_OVGoMNR0P2li6xjYkjwjalUhckB7GjMPcL95Rc7X-ucGXma2Uixyw9y4SBkDGDpty_"
-
     resp = push_alert(_token, _title, _msg_body)
     print(resp)
