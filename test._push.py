cfe842b67a765d9f2b45fb50bac3a4c65ac301d5
tehnplk <tehnplk@gmail.com>
2021-03-20 13:36:52 +0700

    tehnn

--- test._push.py
+++ test._push.py
@@ -1,8 +1,8 @@
 from pyfcm import FCMNotification
 
-api_key = "AAAA1bq8cok:APA91bEwnfLAUz5FQaFbCj9SB8iDkpKBYO78vZ9Ar3jx_9F8RwsjRLDRR4jHZGaEgUWxDiTYsynwv2PNhkSVsxnSRr0a37JBmi1RAoa2bVZ0d5wnA_saDViOcFcS_tOwQp-CRmMIcvwS"
+server_key = "AAAA1bq8cok:APA91bEwnfLAUz5FQaFbCj9SB8iDkpKBYO78vZ9Ar3jx_9F8RwsjRLDRR4jHZGaEgUWxDiTYsynwv2PNhkSVsxnSRr0a37JBmi1RAoa2bVZ0d5wnA_saDViOcFcS_tOwQp-CRmMIcvwS"
 
-push_service = FCMNotification(api_key=api_key)
+push_service = FCMNotification(api_key=server_key)
 data_message = {
     "click_action": "FLUTTER_NOTIFICATION_CLICK",
     "id": "1",
@@ -35,6 +35,6 @@ if __name__ == '__main__':
     _title = f"ทดสอบ อีก 10 คิวจะถึงคิวของท่าน"
     _msg_body = f"""หมายเลข Q001 กรุณาไปรอที่บริเวณ จุดซักประวัติ"""
     _token = "depeVgO_f7y0FLgJQSejzd:APA91bFcnVNNjKa1fy5jRsR3Vpsx0_3uONWKzoZ9fvxjrO5zZTH7z41kecMLLLurNDCmSf28vOSc2KBghKaR0n5-1cjq6etfHLGFPm_VaOhUZB6hYIqJFuE57ZQLpuz0Ufjrr8LIENSH"
-    # resp = single_push_alert(_token, _title, _msg_body)
+    resp = single_push_alert(_token, _title, _msg_body)
     resp = multi_push_alert([_token], _title, _msg_body)
     print(resp)
